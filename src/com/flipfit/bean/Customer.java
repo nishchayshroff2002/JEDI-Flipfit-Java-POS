60d1a41baaef56335fad70a8e668a0ffeaa226df
shikhar-ag02 <shikhar.ag02@gmail.com>
2024-10-09 12:06:34 +0530

    Adds Customer model

--- src/com/flipfit/bean/Customer.java
+++ src/com/flipfit/bean/Customer.java
@@ -1,5 +1,34 @@
 package com.flipfit.bean;
 
-public class Customer {
+public class Customer extends User {
+
+
+    private String customerPhone;
+    private String cardDetails;
+
+    public Customer() {
+    }
+
+    public Customer(String userId, String userName, String email, String password, String customerPhone, String cardDetails) {
+        super(userId, userName, email, password, Role.CUSTOMER);
+        this.customerPhone = customerPhone;
+        this.cardDetails = cardDetails;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getCardDetails() {
+        return cardDetails;
+    }
+
+    public void setCardDetails(String cardDetails) {
+        this.cardDetails = cardDetails;
+    }
 
 }
