eb75507ec6bbddf4c50e10b853bf84aae3e63acf
nakxtra <nakshatrakataria@gmail.com>
2024-10-09 11:32:24 +0530

    adds admin model

--- src/com/flipfit/bean/Admin.java
+++ src/com/flipfit/bean/Admin.java
@@ -1,4 +1,32 @@
 package com.flipfit.bean;
 
 public class Admin {
+    private int userId;
+    private String password;
+    private String emailID;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
 }
