11e8e020cb949040f1270e86e4212ccd0abec589
Sneha920 <snehachowdary6767@gmail.com>
2024-10-09 14:06:53 +0530

    modified main file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import com.flipfit.business.CustomerBusiness;
+import com.flipfit.business.CustomerInterface;
+
+import java.text.SimpleDateFormat;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        CustomerInterface business = new CustomerBusiness();
+        business.login("Sneha","Sneha@123");
+        business.logout();
+        System.out.println("Register a customer :"+business.register());
+        System.out.println("View all Bookings: "+business.viewAllBookings());
+        System.out.println("View all Gyms : "+business.viewAllGyms()    );
+        System.out.println("View all Bookings : "+business.viewAllBookings());
+        System.out.println("Book a slot: "+business.bookSlot(1,1));
+        business.cancelBooking(1);
+        business.viewMyProfile(1);
     }
 }
